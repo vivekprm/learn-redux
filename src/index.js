37e9b65cff725610e838625c9b9be96b1595b07f
Vivek Mishra <v.mishra@f5.com>
2020-10-20 17:02:08 +0530

    Redux basic setup with initial State

--- src/index.js
+++ src/index.js
@@ -1,42 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {createStore} from 'redux';
+import allReducer from "./reducers";
+import { createStore } from 'redux';
 
-// STORE -> Globalized State
-// Action -> INCREMENT: simple function return object.
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
-// Reducer -> Based on the action it modifies the state.
-const counter = (state=0, action) => {
-  switch(action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;  
-  }
-}
-
-let store = createStore(counter);
-
-// Display it in console
-store.subscribe(() => console.log(store.getState()));
-
-// Dipatch -> Send action to reducer
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
+const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <React.StrictMode>
